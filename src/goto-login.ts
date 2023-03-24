9e45b906ea8709b53c439157512002e3ec3efa1e
Eric <eriliu@ucdavis.edu>
2023-03-24 12:51:10 -0700

    `goto-login.ts`: Traverse DOM instead of hardcoding link

--- src/goto-login.ts
+++ src/goto-login.ts
@@ -1,2 +1,12 @@
-// Redirects from https://login.canvas.ucdavis.edu/, saving you a click or two.
-window.location.replace("https://canvas.ucdavis.edu/login/saml")
+(function () {
+    const links = document.querySelectorAll("a.marketing-highlight") as NodeListOf<HTMLAnchorElement>
+    for (const link of links) {
+        const text = link.querySelector("h3 > span")!.innerHTML
+        // Text should be "UCD Log In"
+        if (text.search("UCD") == -1) {
+            continue
+        }
+
+        link.click()
+    }
+}())
