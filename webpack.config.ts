75361b6f1cffed9d97a5fc117013d6601804e761
Eric <eriliu@ucdavis.edu>
2023-03-30 12:09:23 -0700

    Reduce nesting in Webpack config

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,32 +2,37 @@ import path from "path"
 import wp from "webpack"
 
 class GenerateInjectorPlugin {
+    name = this.constructor.name
+    compile?: wp.Compilation
+
+    onProcessAssets() {
+        const hook = this.compile?.getAsset("ui/hook.js")!
+        const source = hook.source.buffer().toString()
+        // Escape format string
+        source.replaceAll("`", "\\`")
+        const asset = new wp.sources.RawSource(`window.eval?.(\`${source}\`)`)
+        this.compile?.emitAsset("ui/inject.js", asset)
+    }
+
+    onCompile(compile: wp.Compilation) {
+        this.compile = compile
+        compile.hooks.processAssets.tap(
+            this.name,
+            this.onProcessAssets.bind(this)
+        )
+    }
+
     apply(compiler: wp.Compiler) {
         compiler.hooks.compilation.tap(
-            this.constructor.name,
-            (compile) => {
-                compile.hooks.processAssets.tap({
-                    name: this.constructor.name,
-                    stage: wp.Compilation.PROCESS_ASSETS_STAGE_ADDITIONAL
-                }, async () => {
-                    try {
-                        const hook = await compile.getAsset("hook.js")!.source.buffer().toString()
-                        // Escape format string
-                        hook.replaceAll("`", "\\`")
-                        compile.emitAsset("inject.js", new wp.sources.RawSource(`window.eval?.(\`${hook}\`)`))
-                    }
-                    catch (err) {
-                        console.log(err)
-                    }
-                })
-            }
+            this.name,
+            this.onCompile.bind(this)
         )
     }
 }
 
 function getEntries(): wp.EntryObject {
     // JS
-    const scripts = ["auto/goto", "auto/submit", "hook", "setup"]
+    const scripts = ["auto/goto", "auto/submit", "ui/hook", "setup"]
     const entries: wp.EntryObject = {}
     for (const script of scripts) {
         entries[script] = `./ts/${script}.ts`
