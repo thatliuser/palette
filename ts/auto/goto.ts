6c5ed2a5809283f5798c719ad611138121a00c32
Eric <eriliu@ucdavis.edu>
2023-03-30 12:14:01 -0700

    Remove IIFEs from `ts/auto`

--- ts/auto/goto.ts
+++ ts/auto/goto.ts
@@ -1,13 +1,11 @@
 // Autoclick the login button on the homepage.
-(() => {
-    const links = document.querySelectorAll("a.marketing-highlight") as NodeListOf<HTMLAnchorElement>
-    for (const link of links) {
-        const text = link.querySelector("h3 > span")!.innerHTML
-        // Text should be "UCD Log In"
-        if (text.search("UCD") == -1) {
-            continue
-        }
-
-        link.click()
+const links = document.querySelectorAll("a.marketing-highlight") as NodeListOf<HTMLAnchorElement>
+for (const link of links) {
+    const text = link.querySelector("h3 > span")!.innerHTML
+    // Text should be "UCD Log In"
+    if (text.search("UCD") == -1) {
+        continue
     }
-})()
+
+    link.click()
+}
--- ts/auto/submit.ts
+++ ts/auto/submit.ts
@@ -1,12 +1,10 @@
 // Autosubmit CAS stuff if autofilled
-(() => {
-    // Make username and password fields required
-    // This way the page won't reload and the clicking won't infinitely loop
-    // Also it's just generally more ergonomic
-    for (const id of ["username", "password"]) {
-        const input = document.querySelector(`input#${id}`) as HTMLInputElement
-        input.setAttribute("required", "")
-    }
-    const submit = document.querySelector("input#submit") as HTMLInputElement
-    submit.click()
-})()
+// Make username and password fields required
+// This way the page won't reload and the clicking won't infinitely loop
+// Also it's just generally more ergonomic
+for (const id of ["username", "password"]) {
+    const input = document.querySelector(`input#${id}`) as HTMLInputElement
+    input.setAttribute("required", "")
+}
+const submit = document.querySelector("input#submit") as HTMLInputElement
+submit.click()
